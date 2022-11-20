9e5c0288b11599d7e52a5d66126a06d31e86efa5
menooa25 <menooa2013@gmail.com>
2022-11-20 08:37:33 +0330

    serializer unique together validation removed

--- accounts/views.py
+++ accounts/views.py
@@ -17,12 +17,3 @@ class AccountRetrieve(ListAPIView, UpdateAPIView):
     def get_object(self):
         user = self.request.user
         return get_object_or_404(self.queryset, id=user.id)
-
-# class AccountPasswordUpdate(UpdateAPIView, ):
-#     queryset = Account.objects.all()
-#     serializer_class = AccountSerializer
-#     permission_classes = [IsAuthenticated]
-#
-#     def get_object(self):
-#         user = self.request.user
-#         return self.queryset.get_object_or_404(id=user.id)
